ee11728a1cd071f7f659061f5e634267f93c5b2f
Vlad-Stefan Harbuz <vlad@vlad.website>
2026-02-11 12:34:24 +0000

    Every.org button: centralise URL, remove PayPal, remove Venmo

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+export const EVERYORG_BUTTON_URL = 'https://www.every.org/osendowment?method=card%2Cbank%2Cpay%2Cgift%2Cstocks%2Cdaf%2Ccrypto&suggestedAmounts=100%2C500%2C1000%2C2000&designation=Endowment+Fund&utm_campaign=donate-link#/donate';
+
 export function humanize(n) {
     const thousand = 1000;
     const million = 1000000;
