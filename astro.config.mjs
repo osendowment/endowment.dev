8096151f640b064e6aef61444fe27ea6dfdde960
Vlad-Stefan Harbuz <vlad@vlad.website>
2026-02-16 18:22:59 +0000

    inline css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import playformInline from '@playform/inline';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://endowment.dev',
@@ -11,9 +13,7 @@ export default defineConfig({
             hostname: 'data.endowment.dev',
         }],
     },
-    integrations: [
-        sitemap({
-            filter: (page) => !page.includes('/thank-you') && !page.includes('/404'),
-        }),
-    ],
+    integrations: [sitemap({
+        filter: (page) => !page.includes('/thank-you') && !page.includes('/404'),
+    }), playformInline()],
 });
