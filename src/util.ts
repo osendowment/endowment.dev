1d12a6b88eeed6ac580dc182fb8c27356619a38c
Vlad-Stefan Harbuz <vlad@vlad.website>
2026-02-25 20:06:10 +0000

    Every.org: add require_share_info=true

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,4 @@
-export const EVERYORG_BUTTON_URL = 'https://www.every.org/osendowment?method=card%2Cbank%2Cpay%2Cstocks%2Cdaf%2Ccrypto&suggestedAmounts=100%2C500%2C1000%2C2000&designation=Endowment+Fund&utm_campaign=donate-link#/donate';
+export const EVERYORG_BUTTON_URL = 'https://www.every.org/osendowment?method=card%2Cbank%2Cpay%2Cstocks%2Cdaf%2Ccrypto&suggestedAmounts=100%2C500%2C1000%2C2000&designation=Endowment+Fund&utm_campaign=donate-link&require_share_info=true#/donate';
 
 export function humanize(n) {
     const thousand = 1000;
